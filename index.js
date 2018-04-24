2dad2b0c6ba406098fec7aa15cc1cae1d368c250
Ethan Sternke <ethan.sternke@gmail.com>
2018-04-24 14:39:44 -0400

    added server functionality

--- index.js
+++ index.js
@@ -5,6 +5,41 @@ const app = express()
 app.use(express.static('public'))
 app.use(express.json())
 
-// Fill in your request handlers here
+app.get('/hi', (req, res) => {
+    res.send("hello");
+})
+
+let updateHistory = [];
+let newUpdates = [];
+
+app.post('/updates', (req, res) => {
+    // console.log("POST received: " + JSON.stringify(req.body));
+    // updates.push(req.body.clientUpdates);
+    for (let i in req.body.clientUpdates) {
+        updateHistory.push(req.body.clientUpdates[i]);
+    }
+
+    newUpdates = [];
+    for (let i in updateHistory) {
+        if (updateHistory[i][3] > req.body.after) {
+            newUpdates.push(updateHistory[i]);
+        }
+    }
+
+    // console.log(updateHistory);
+    // console.log(newUpdates);
+
+    if (req.body.history === "no") {
+        res.send({
+            updates: newUpdates,
+        })
+    } else {
+        res.send({
+            updates: updateHistory
+        })
+    }
+
+    // res.send({updates: updateHistory});
+})
 
 app.listen(port)
--- public/polling.js
+++ public/polling.js
@@ -1,1 +1,57 @@
 // Add logic to this script to poll server every second for updated pixels.
+
+let timeStamp = Date.now();
+
+function fetchHistory() {
+    fetch("/updates", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                clientUpdates: clientUpdates,
+                after: timeStamp,
+                history: "yes"
+            }),
+        })
+        .then(res => res.json())
+        .then(res => {
+            // console.log(res.updates);
+            for (let i in res.updates) {
+                bitmap.setColor(res.updates[i][0], res.updates[i][1], res.updates[i][2]);
+                timeStamp = res.updates[i][3];
+            }
+            clientUpdates = [];
+            // timeStamp = Date.now();
+        })
+}
+
+function fetchUpdates() {
+    fetch("/updates", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                clientUpdates: clientUpdates,
+                after: timeStamp,
+                history: "no"
+            }),
+        })
+        .then(res => res.json())
+        .then(res => {
+            // console.log(res.updates);
+            for (let i in res.updates) {
+                bitmap.setColor(res.updates[i][0], res.updates[i][1], res.updates[i][2]);
+                timeStamp = res.updates[i][3];
+            }
+            clientUpdates = [];
+            // timeStamp = Date.now();
+            setTimeout(fetchUpdates, 1000);
+        })
+}
+
+fetchUpdates();
+fetchHistory();
+
+// this.setColor(row, col, paint_color);
